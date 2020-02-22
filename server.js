79546077789c62d1c81b305abb281b27c2f73a5a
Don Dean <Dondean3030@gmail.com>
2020-02-21 22:35:24 -0700

    maybe fixed server

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Send every request to the React app
-// Define any API routes before this runs
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
 });
